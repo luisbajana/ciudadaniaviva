42a7abdb825f910f8bf65ab56a12ef8e477b77bd
Luis Bajana <lbajana@gmail.com>
2014-12-04 11:34:12 -0300

    Paperclip second shot

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,6 @@ Ciudadaniaviva::Application.configure do
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-      :region => "Oregon"
     }
   }
 end
